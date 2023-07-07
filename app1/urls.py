bb79afb97aad7a794a335c43ddd8cd5836b6a05b
saipa <saipavan861@gmail.com>
2023-07-07 14:37:12 +0530

    login added

--- app1/urls.py
+++ app1/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 path('hello/', views.hello),
     path('', views.mail),
 path('send/', views.send),
+    path('logout/',views.page_logout),
+path('login/',views.login),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,19 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .utilities import mail_send,send_whatsApp_message
 from .models import Messagesdetails,Customers
 import datetime
 from django.utils import timezone
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
+def login(request):
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('/login')
+
+    else:
+        return render(request,'login.html')
+
+def page_logout():
+    pass
 
 def hello(request):
     return  HttpResponse('hello p3')
 
+
 # Create your views here.
+
+@login_required(login_url='/login/')
 def send(request):
-    # mobile = request.GET['mobilenumber']
-    # gmail=request.GET['email']
-    # message = request.GET['subject']
 
     mobile = request.POST.get("mobilenumber")
     gmail = request.POST.get("email")
@@ -26,10 +49,14 @@ def send(request):
                                                   email =gmail,message = message,sendingtime = datetime.datetime.now(tz=timezone.utc))
 
     return HttpResponse("""<h>message sended successfully </h> 
-    <a href="/sendMessages/index">Home page link</a>""")
+    <a href="/">Home page link</a>""")
 
+@login_required(login_url='/login/')
+def page_logout(request):
+    logout(request)
+    return redirect('/login/')
 
-
+@login_required(login_url='/login/')
 def mail(request):
 
     if request.POST.get("customername"):
@@ -38,7 +65,7 @@ def mail(request):
 
         for i in customerDetails:
             mobile,email = i.mobile,i.email
-        print(mobile,email)
+
         custometList = Customers.objects.all()
         return render(request, "index.html", {'customername':customername,'custometList':custometList,'mobile':mobile,'email':email})
     custometList = Customers.objects.all()
